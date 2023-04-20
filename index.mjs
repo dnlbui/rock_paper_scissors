3b38490bbbca2be18ddb839d8330f2a5c68d6492
dnlbui <dnlbui@proton.me>
2023-04-20 16:23:29 -0500

    logic for for rock pape

--- index.mjs
+++ index.mjs
@@ -1,19 +1,32 @@
-import { loadStdlib } from '@reach-sh/stdlib';
-import * as backend from './build/index.main.mjs';
-const stdlib = loadStdlib();
+import { loadStdlib } from '@reach-sh/stdlib'; /// imports the Reach standard library
+import * as backend from './build/index.main.mjs'; // imports the compiled Reach program/ backend
+const stdlib = loadStdlib(); // loads the standard library dynamically based on the REACH_CONNECTOR_MODE environment variable
 
 const startingBalance = stdlib.parseCurrency(100);
-const accAlice = await stdlib.newTestAccount(startingBalance);
-const accBob = await stdlib.newTestAccount(startingBalance);
+const accAlice = await stdlib.newTestAccount(startingBalance); // creates a new test account with the specified starting balance
+const accBob = await stdlib.newTestAccount(startingBalance); // only work on Reach-provided testnets
 
-const ctcAlice = accAlice.contract(backend); // deploy contract
-const ctcBob = accBob.contract(backend, ctcAlice.getInfo()); // attach to it
+const ctcAlice = accAlice.contract(backend); // has Alice deploy the app 
+const ctcBob = accBob.contract(backend, ctcAlice.getInfo()); // has Bob join the app. The second argument is the app's info object which is used to connect the two apps together
 
+const HAND = ['Rock', 'Paper', 'Scissors'];
+const OUTCOME = ['Bob wins', 'Draw', 'Alice wins'];
+const Player = (Who) => ({
+  getHand: () => {
+    const hand = Math.floor(Math.random() * 3);
+    console.log(`${Who} played ${HAND[hand]}`);
+    return hand;
+  },
+  seeOutcome: (outcome) => {
+    console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
+  },
+});
+// initializes backend for Alice and Bob 
 await Promise.all([
   ctcAlice.p.Alice({
-    // implement Alice's interact object here
+    ...Player('Alice'),
   }),
   ctcBob.p.Bob({
-    // implement Bob's interact object here
+    ...Player('Bob'),
   }),
 ]);
--- index.rsh
+++ index.rsh
@@ -1,15 +1,36 @@
-'reach 0.1'
+'reach 0.1';
+
+const Player = {
+  getHand: Fun([], UInt),
+  seeOutcome: Fun([UInt], Null),
+};
 
 export const main = Reach.App(() => {
   const Alice = Participant('Alice', {
-    //Specify Alice's interact interface here
+    //interact interface is defined in the Reach standard library and is used to interact with the user in the browser or the terminal depending on the environment the program is run in. 
+    ...Player, // specify Alice's interact interface here
   });
-  const Bob = Participant('Bob', {
-    // Specify Bob's interact interface here
+  const Bob   = Participant('Bob', {
+    ...Player, // specify Bob's interact interface here
   });
   init();
   // write your program here
 
+  Alice.only(() => {
+    const handAlice = declassify(interact.getHand());
+  });
+  Alice.publish(handAlice);
+  commit();
+
+  Bob.only(() => {
+    const handBob = declassify(interact.getHand());
+  });
+  Bob.publish(handBob);
+
+  const outcome = (handAlice + (4 - handBob)) % 3;
+  commit();
+
+  each([Alice, Bob], () => {
+    interact.seeOutcome(outcome);
+  });
 });
-
-
